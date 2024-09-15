1c633c4c961d5af1ab1c736b7a9a1de217347f30
Peter Bjorklund <piot@hotmail.com>
2024-09-15 10:41:46 +0200

    fix: missing Clone

--- steps/tests/types.rs
+++ steps/tests/types.rs
@@ -2,7 +2,7 @@
  * Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/nimble-rust/workspace
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  */
-#[derive(Debug, PartialEq, Eq)] // Debug is needed for asserts in tests
+#[derive(Debug, Clone, PartialEq, Eq)] // Debug is needed for asserts in tests
 pub enum GameInput {
     Jumping(bool),
     MoveHorizontal(i32),
