25a732d1d6aa5a1643bd9e646bcf57f171f6bfba
Peter Bjorklund <piot@hotmail.com>
2024-09-24 17:12:04 +0200

    chore: add missing header

--- crates/connection-layer/src/datagram_builder.rs
+++ crates/connection-layer/src/datagram_builder.rs
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) Peter Bjorklund. All rights reserved. https://github.com/nimble-rust/workspace
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ */
 use crate::{verify_hash, write_to_stream, ConnectionId, ConnectionSecretSeed};
 use datagram::{DatagramDecoder, DatagramEncoder};
 use flood_rs::prelude::{InOctetStream, OutOctetStream};
